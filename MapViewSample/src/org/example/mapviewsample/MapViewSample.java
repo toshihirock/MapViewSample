488b06cd32fce441553fcc3665e7cba5f8d1abfc
toshihirock <toshihiro308@gmail.com>
2011-06-26 10:17:54 +0900

    Add search address

--- MapViewSample/src/org/example/mapviewsample/MapViewSample.java
+++ MapViewSample/src/org/example/mapviewsample/MapViewSample.java
@@ -1,6 +1,9 @@
 package org.example.mapviewsample;
 
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -8,10 +11,14 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.Projection;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MapViewSample extends MapActivity {
 
@@ -28,7 +35,7 @@ public class MapViewSample extends MapActivity {
 		// When map tap, display zoom controller
 		mapView.setBuiltInZoomControls(true);
 		
-	   ConcreteOverlay overlay = new ConcreteOverlay();
+	   ConcreteOverlay overlay = new ConcreteOverlay(this);
 	   
 	   List<Overlay> overlayList = mapView.getOverlays();
 	   overlayList.add(overlay);
@@ -57,11 +64,16 @@ public class MapViewSample extends MapActivity {
 		
 		Paint mCirclePaint;
 		
-		ConcreteOverlay() {
+		// exchange adress to location
+		Geocoder mGeocoder;
+		
+		ConcreteOverlay(Context context) {
 			mGeopoint = null;
 			mCirclePaint = new Paint();
 			mCirclePaint.setStyle(Paint.Style.FILL);
 			mCirclePaint.setARGB(255, 255, 0, 0);
+			
+			mGeocoder = new Geocoder(context, Locale.JAPAN);
 		}
 		
 		/**
@@ -69,6 +81,42 @@ public class MapViewSample extends MapActivity {
 		 */
 		public boolean onTap(GeoPoint point, MapView mapView) {
 			mGeopoint = point;
+			
+			// 画面上部のTextViewを取得
+			TextView textView = (TextView)findViewById(R.id.TextView01);
+			
+			// 市町村名まで取得出来たか
+			boolean success = false;
+			
+			try {
+				// 緯度経度から住所への変換処理
+				List<Address> addressList = mGeocoder.getFromLocation(point.getLatitudeE6() / 1E6, point.getLongitudeE6() / 1E6, 5);
+				
+				for(Iterator<Address> it=addressList.iterator(); it.hasNext();) {
+					Address address = it.next();
+					
+					String country = address.getCountryName();
+					String admin = address.getAdminArea();
+					String locality = address.getLocality();
+					
+					if(country != null && admin != null && locality != null) {
+						textView.setText(country + admin + locality);
+						success = true;
+						break;
+					}
+				}
+				
+				if(!success) {
+					textView.setText("Error");
+				}
+				
+				textView.invalidate();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 			return super.onTap(point, mapView);
 		}
 		
